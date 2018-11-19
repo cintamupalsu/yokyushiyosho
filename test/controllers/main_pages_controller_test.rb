f4a3a1495b1ff9ca341f70e51a8e2400cdf4d4eb
maulanamania <maulanamania@gmail.com>
2018-11-19 05:23:14 +0000

    Finish static pages

---
+++ test/controllers/main_pages_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class MainPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "PK-Tools on Watson"
+  end
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
+  test "should get home" do
+    get main_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get main_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+  
+  test "should get about" do
+    get main_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+end
