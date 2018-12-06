2522cbf256662730e4244608454383ff525fa216
maulanamania <maulanamania@gmail.com>
2018-12-06 08:03:50 +0000

    fix sakusei.html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_043011) do
+ActiveRecord::Schema.define(version: 2018_11_29_083413) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
